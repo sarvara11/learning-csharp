980704628f9a77f44bcdb0bc96ac14b9c707848a
sarvara11 <sarvaragulomova1@gmail.com>
2026-02-03 23:57:58 +0500

    Add files via upload
    Add first C# variables example

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Variables are really important. They allow you to store data inside your memory
+// and reuse it in your code every single time that u need it.
+// declare a variable
+string myFriendsName;
+// assign a value to the variable
+myFriendsName = "John";
+
+// use/access the variable
+Console.WriteLine(myFriendsName);
+
+// overwriting the variable value
+myFriendsName = "Jane";
+Console.WriteLine(myFriendsName);
+
+Console.ReadKey();
+
+// we can create and assign value to a variable in one line
+string myFriendsName2 = "Frank";
